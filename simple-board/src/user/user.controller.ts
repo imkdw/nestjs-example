07ac159df99e534b5b0511e39ee943326b9da028
imkdw <imkdw@kakao.com>
2023-10-07 23:59:44 +0900

    message

--- simple-board/src/user/user.controller.ts
+++ simple-board/src/user/user.controller.ts
@@ -1,4 +1,7 @@
 import { Controller } from '@nestjs/common';
+import UserService from './user.service';
 
 @Controller('user')
-export default class UserController {}
+export default class UserController {
+  constructor(private readonly userService: UserService) {}
+}
