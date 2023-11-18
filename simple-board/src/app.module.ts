79c4c0b06b5f52f4dc3415f85ffd94f1b657510e
imkdw <imkdw@kakao.com>
2023-11-18 23:36:44 +0900

    simple-board 프로젝트 삭제

--- simple-board/src/app.module.ts
+++
@@ -1,8 +0,0 @@
-import { Module } from '@nestjs/common';
-import AuthModule from './modules/auth/auth.module';
-import BoardModule from './modules/board/board.module';
-
-@Module({
-  imports: [AuthModule, BoardModule],
-})
-export default class AppModule {}
--- simple-board/src/user/user.controller.ts
+++
@@ -1,7 +0,0 @@
-import { Controller } from '@nestjs/common';
-import UserService from './user.service';
-
-@Controller('user')
-export default class UserController {
-  constructor(private readonly userService: UserService) {}
-}
